ae16ab024456ce18860faee9ce97ee422178de57
Nastya <nastya.silch@gmail.com>
2019-11-27 20:48:50 +0300

    update update code

--- Module_1/ohlc.py
+++ Module_1/ohlc.py
@@ -12,22 +12,22 @@ df['Time'] = pd.to_datetime(df['DateTime']).dt.time
 df = df[(df['Time'] >= datetime.time(7,0,0) ) & (df['Time'] <= datetime.time(23,59,59,999999)) |
         (df['Time'] >= datetime.time(0,0,0) ) & (df['Time'] <= datetime.time(2,59,59,999999) )]
 
-
 df['DateTime'] = pd.to_datetime(df['DateTime'])
 
 df = df.set_index('DateTime')
 df = df.drop(columns= ['Dates','Time', 'Amount'])
 
 
-def trades_5(df1):
-    print( '5MIN DATA' )
+def trades(df1):
+    tf = input('INput timeframe')
+    tf += 'Min'
     num_tickets = int(input('Input number of tickets: '))
     data = pd.DataFrame()
     for i in range(num_tickets):
         ticket = input('Input name of Tickets: ')
         t = ticket
         ticket = df1[df1['Company'] == ticket]
-        ticket = ticket['Price'].resample('5Min').ohlc()
+        ticket = ticket['Price'].resample(tf).ohlc()
         ticket['Company'] = t
         data = data.append(ticket)
     data = data.query("open != 'NaN'")
@@ -35,44 +35,5 @@ def trades_5(df1):
     data = data.reset_index()
     data = data.set_index('Company')
     data = data.reset_index()
-    return data
+    return data.to_csv()
 
-def trades_30(df1):
-    print( '30MIN DATA' )
-    num_tickets = int(input('Input number of tickets: '))
-    data = pd.DataFrame()
-    for i in range(num_tickets):
-        ticket = input('Input name of Tickets: ')
-        t = ticket
-        ticket = df1[df1['Company'] == ticket]
-        ticket = ticket['Price'].resample('30Min').ohlc()
-        ticket['Company'] = t
-        data = data.append(ticket)
-    data = data.query("open != 'NaN'")
-    data.sort_values(["DateTime"], inplace=True)
-    data = data.reset_index()
-    data = data.set_index('Company')
-    data = data.reset_index()
-    return data
-
-def trades_240(df1):
-    print( '240MIN DATA' )
-    num_tickets = int(input('Input number of tickets: '))
-    data = pd.DataFrame()
-    for i in range(num_tickets):
-        ticket = input('Input name of Tickets: ')
-        t = ticket
-        ticket = df1[df1['Company'] == ticket]
-        ticket = ticket['Price'].resample('240Min').ohlc()
-        ticket['Company'] = t
-        data = data.append(ticket)
-    data = data.query("open != 'NaN'")
-    data.sort_values(["DateTime"], inplace=True)
-    data = data.reset_index()
-    data = data.set_index('Company')
-    data = data.reset_index()
-    return data
-
-print(trades_5(df))
-print(trades_30( df))
-print(trades_240(df))
