ded9bcf6991bb8e603c464dd5748701123f09e2c
Nastya <nastya.silch@gmail.com>
2019-11-30 05:09:07 +0300

    add --help

--- Module_1/ohlc.py
+++ Module_1/ohlc.py
@@ -5,10 +5,35 @@ import pandas as pd
 
 
 def createParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', type=argparse.FileType())
-    parser.add_argument('-tf', '--timeframe', type=str)
-    parser.add_argument('-t', '--tickets', type=str, nargs='+')
+    parser = argparse.ArgumentParser(
+        prog='ohlc.py',
+        description='''Finally, I done this program. I did not sleep night, but i did this''',
+        epilog='''(c) Author of this program stupid and irresponsible ''',
+        add_help=False
+    )
+
+    parent_group = parser.add_argument_group(title='Команды')
+
+    parent_group.add_argument('--help', '-h', action='help',
+                              help='Справка')
+
+    parent_group.add_argument('--full info', '-fi', help='Информация о используемых функциях')
+
+    file_group = parser.add_argument_group(title='Параметры')
+
+    file_group.add_argument('--file', '-f',
+                            help='Путь к файлу',
+                            type=argparse.FileType())
+
+    file_group.add_argument('--timeframe', '-tf',
+                            help='Маштаб свечного графика',
+                            metavar='ВРЕМЯ',
+                            type=str)
+
+    file_group.add_argument('--tickets', '-t',
+                            help='Название тикетов',
+                            type=str,
+                            nargs='+')
 
     return parser
 
@@ -19,11 +44,9 @@ if __name__ == '__main__':
 
     print(namespace)
 
-    text = namespace.name
-    numTickets = namespace.numTickets
-    timeframe = namespace.timeframe
+    text = namespace.file
     tickets = namespace.tickets
-
+    timeframe = namespace.timeframe
     df = pd.read_csv(text, names=['Company', 'Price', 'Amount', 'DateTime'])
 
 
@@ -38,16 +61,16 @@ df = df.set_index('DateTime')
 df = df.drop(columns=['Dates', 'Time', 'Amount'])
 
 
-def trades(df1, timeframe1, tickets1):
-    timeframe1 += 'Min'
+def trades(df1, timeframe, tickets):
+    timeframe += 'Min'
     data = pd.DataFrame()
-    for i in range(len(tickets1)):
-        t = tickets1[i]
-        tickets1[i] = df1[df1['Company'] == tickets1[i]]
-        tickets1[i] = pd.DataFrame(tickets1[i])
-        tickets1[i] = tickets1[i]['Price'].resample(timeframe1).ohlc()
-        tickets1[i]['Company'] = t
-        data = data.append(tickets1[i])
+    for i in range(len(tickets)):
+        t = tickets[i]
+        tickets[i] = df1[df1['Company'] == tickets[i]]
+        tickets[i] = pd.DataFrame(tickets[i])
+        tickets[i] = tickets[i]['Price'].resample(timeframe).ohlc()
+        tickets[i]['Company'] = t
+        data = data.append(tickets[i])
     data = data.query("open != 'NaN'")
     data.sort_values(["DateTime"], inplace=True)
     data = data.reset_index()
